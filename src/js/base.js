a743fc43295378b7fce890014908aa0442cacfe4
developer485 <asram517@gmail.com>
2020-01-21 22:51:13 +0530

    src and prod uploaded

---
+++ src/js/base.js
@@ -0,0 +1,13 @@
+const base = {
+    winner: document.querySelector('.winner'),
+    rock: document.querySelector('.rock'),
+    paper: document.querySelector('.paper'),
+    scissor: document.querySelector('.scissor'),
+    playerScore: document.querySelector('.player-score'),
+    computerScore: document.querySelector('.computer-score'),
+    scoreTie: document.querySelector('.score-tie'),
+    computerRender: document.querySelector('.box-2'),
+    row: document.querySelector('.row')
+}
+
+export default base;
